5609b4f0c4fa5044de8c226894206740d7c448e3
Viktoriia <viktoriamyhailiak@gmail.com>
2025-04-22 21:22:12 +0300

    make build

---
+++ index.e13072f1.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper--mobile",{slidesPerView:1,spaceBetween:16,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".swiper--tablet",{slidesPerView:2,spaceBetween:20,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.e13072f1.js.map
